9f8f8c9049eb9e8a012f370572e44b66d53ab359
Anand Singh <126552761+anand7733@users.noreply.github.com>
2023-03-27 23:57:08 +0530

    Star patern
    *
    **
    ***
    ****
    ***
    **
    *

--- StarPattern.java
+++ StarPattern.java
@@ -1,22 +1,22 @@
-public class StarPattern {
-    public void main(String[]args)
-    {
-        for(int i=1;i<=5;i++)
+public class StarPattern 
+{
+            public static void main (String[]args)
         {
-            for(int j=1;j<=i;j++)
+            for(int i=1;i<=5;i++)
             {
-                System.out.println("*");
+                for(int j=1;j<=i;j++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-        for(int i=1;i<=5;i++)
-        {
-            for(int j=3;j>=i;j--)
+            for(int i=1;i<=5;i++)
             {
-                System.out.println("*");
+                for(int j=4;j>=i;j--)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
-    }
-    
-}
+ }
